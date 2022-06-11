9d54c09280d7becad0f0fb3da92958b9a63a97eb
Patrick Zhu <patrick140814@outlook.com>
2022-06-11 19:46:50 +0800

    2022/6/11-19:46
    Signed-off-by: Patrick Zhu <patrick140814@outlook.com>

---
+++ src/main/java/com/aim/questionnaire/controller/QuestionnaireController.java
@@ -0,0 +1,112 @@
+package com.aim.questionnaire.controller;
+
+import com.aim.questionnaire.beans.HttpResponseEntity;
+import com.aim.questionnaire.common.Constans;
+import com.aim.questionnaire.dao.entity.QuestionnaireEntity;
+import com.aim.questionnaire.service.QuestionnaireService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Patrick Zhu
+ * @since 2022.6.11
+ */
+@RestController
+public class QuestionnaireController {
+
+    private final Logger logger = LoggerFactory.getLogger(QuestionnaireController.class);
+
+    @Autowired
+    private QuestionnaireService questionnaireService;
+
+    /**
+     * 查询全部问卷的信息
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    @RequestMapping(value = "/queryQuestionnaireList", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity queryQuestionnaireList(@RequestBody(required = false) QuestionnaireEntity questionnaireEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        List<Object> result = questionnaireService.queryQuestionnaireList(questionnaireEntity);
+        httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+        httpResponseEntity.setData(result);
+        return httpResponseEntity;
+    }
+
+    /**
+     * 根据id删除问卷
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    @RequestMapping(value = "/deleteQuestionnaireById", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity deleteQuestionnaireById(@RequestBody QuestionnaireEntity questionnaireEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            int result = questionnaireService.deleteQuestionnaireById(questionnaireEntity);
+            if (result == -1) {
+                httpResponseEntity.setCode(Constans.EXIST_CODE);
+                httpResponseEntity.setMessage(Constans.PROJECT_EXIST_MESSAGE);
+            } else {
+                httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+                httpResponseEntity.setMessage(Constans.DELETE_MESSAGE);
+            }
+        } catch (Exception e) {
+            logger.info("deleteQuestionnaireById 删除问卷>>>>>>>>>>>" + e.getLocalizedMessage());
+            httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        }
+        return httpResponseEntity;
+    }
+
+    /**
+     * 添加问卷
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    @RequestMapping(value = "/addQuestionnaire", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity addQuestionnaire(@RequestBody QuestionnaireEntity questionnaireEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            int result = questionnaireService.addQuestionnaire(questionnaireEntity, questionnaireEntity.getCreatedBy());
+            if (result == 3) {
+                httpResponseEntity.setCode(Constans.EXIST_CODE);
+                httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+            } else {
+                httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+                httpResponseEntity.setMessage(Constans.ADD_MESSAGE);
+            }
+        } catch (Exception e) {
+            logger.info("addQuestionnaire 创建问卷的基本信息>>>>>>>>>>>" +
+                    e.getLocalizedMessage());
+            httpResponseEntity.setCode(Constans.EXIST_CODE);
+            httpResponseEntity.setMessage(Constans.EXIST_MESSAGE);
+        }
+
+        return httpResponseEntity;
+    }
+
+    /**
+     * 根据问卷id修改问卷
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    @RequestMapping(value = "/modifyQuestionnaireInfo", method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity modifyQuestionnaireInfo(@RequestBody QuestionnaireEntity questionnaireEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+
+        return httpResponseEntity;
+    }
+
+}
---
+++ src/main/java/com/aim/questionnaire/service/QuestionnaireService.java
@@ -0,0 +1,79 @@
+package com.aim.questionnaire.service;
+
+import com.aim.questionnaire.common.utils.UUIDUtil;
+import com.aim.questionnaire.dao.QuestionnaireEntityMapper;
+import com.aim.questionnaire.dao.entity.QuestionnaireEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+/**
+ * @author Patrick Zhu
+ * @since 2022.6.11
+ */
+@Service
+public class QuestionnaireService {
+
+    @Autowired
+    private QuestionnaireEntityMapper questionnaireEntityMapper;
+
+    /**
+     * 添加问卷
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    public int addQuestionnaire(QuestionnaireEntity questionnaireEntity, String user) {
+        String id = UUIDUtil.getOneUUID();
+        questionnaireEntity.setId(id);
+        questionnaireEntity.setProjectId("test");
+
+        int result = questionnaireEntityMapper.insertSelective(questionnaireEntity);
+        return result;
+    }
+
+    /**
+     * 修改问卷
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    public int modifyQuestionnaireInfo(QuestionnaireEntity questionnaireEntity, String user) {
+        return 0;
+    }
+
+    /**
+     * 删除问卷
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    public int deleteQuestionnaireById(QuestionnaireEntity questionnaireEntity) {
+        String questionnaireId = questionnaireEntity.getId();
+        int result = questionnaireEntityMapper.deleteByPrimaryKey(questionnaireId);
+
+        return result;
+    }
+
+    /**
+     * 查询问卷列表
+     *
+     * @param questionnaireEntity
+     * @return
+     */
+    public List<Object> queryQuestionnaireList(QuestionnaireEntity questionnaireEntity) {
+        List<Object> resultList = new ArrayList<Object>();
+        if ("".equals(questionnaireEntity.getQuestionName())) {
+            questionnaireEntity.setQuestionName(null);
+        }
+
+        String id = questionnaireEntity.getId();
+        List<Map<String, Object>> proResult = questionnaireEntityMapper.queryQuestionListByProjectId(id);
+        for (Map<String, Object> proObj : proResult) {
+            resultList.add(proObj);
+        }
+        return resultList;
+    }
+
+}
